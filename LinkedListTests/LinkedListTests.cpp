627daf0dea9ce2043b535c3fd2e127f0139d0556
Danny-Mesics <cybertomasvrbada@gmail.com>
2023-09-08 09:32:02 +1000

    Revert "two tests added"
    This reverts commit 156ab9ace889fffca9a141cd3a5821ba3e884bd2.

--- LinkedListTests/LinkedListTests.cpp
+++ LinkedListTests/LinkedListTests.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Dynamic Array/List.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,32 +9,8 @@ namespace LinkedListTests
 	{
 	public:
 		
-		TEST_METHOD(TestPushBack)
+		TEST_METHOD(TestMethod1)
 		{
-			List testList;
-			testList.PushBack(5);
-			Assert::AreEqual(testList.Last(), 5);
-		}
-
-		TEST_METHOD(TestInsert) 
-		{
-			List testList;
-			for (int i = 0; i < 5; i++) {
-				testList.PushBack(i);
-			}
-
-			for (auto i = testList.Begin(); i != testList.End(); i++) {
-				if (*i == 3) {
-					testList.Insert(i, 14);
-				}
-			}
-
-			for (auto i = testList.Begin(); i != testList.End(); i++) {
-				if (*i == 3) {
-					Assert::AreEqual(*(++i), 14);
-				}
-			}
-
 		}
 	};
 }
